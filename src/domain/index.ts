64f0f614404d90a544ae718aa02fe535233afea9
evuz <jesus_jgb_45@hotmail.com>
2019-05-31 01:12:43 +0200

    feat: create domain

---
+++ src/domain/index.ts
@@ -0,0 +1,24 @@
+import { DepInjection } from 'depsin';
+import { Domain } from 'ts-domain';
+import got from 'got';
+
+import { Inject } from './keys.inject';
+import { IotaPriceUseCase } from './Currency/UseCase/IotaPriceUseCase';
+import { BitfinexRepository } from './Currency/Repositories/BitfinexRepository';
+
+const config = { http: got };
+const domainContainer = new DepInjection(
+  {
+    [Inject.CurrencyRepository]: BitfinexRepository,
+    [Inject.IotaPriceUseCase]: IotaPriceUseCase,
+  },
+  {
+    [Inject.Config]: config,
+  },
+);
+
+const useCases = {
+  iotaPrice: domainContainer.get<IotaPriceUseCase>(Inject.IotaPriceUseCase),
+};
+
+export const domain = new Domain({ useCases, config });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { TelegramBot } from './bot/TelegramBot';
 import { StartFunctionBot } from './bot/listeners/start';
 
+import { domain } from './domain';
+
 const TOKEN = process.env.TELEGRAM_TOKEN;
+domain
+  .get({ useCase: 'iotaPrice' })
+  .execute()
+  .then(v => {
+    console.log(v);
+  });
 
 new TelegramBot([new StartFunctionBot()], TOKEN, { polling: true });
